99ac6d3940e7fb5ba9c5f0c3ac0dbe806025cd4c
Carl <meatballsofmeatballs@gmail.com>
2023-01-04 10:08:01 +0100

    Generator

---
+++ bin/generator.cr
@@ -0,0 +1,19 @@
+require "./generator_help"
+
+class Generator
+  def initialize(exercise)
+    stuff = GeneratorHelp.new(exercise).to_s
+    exercise_snake = to_snake(exercise)
+    file_name = "./exercises/practice/#{exercise}/spec/#{exercise_snake}_spec.cr"
+    file = File.new(file_name, mode = "wb")
+    File.write(file_name, stuff)
+    system("crystal tool format #{file_name}")
+  end
+
+  def to_snake(input)
+    input = input.gsub("-", "_")
+    input
+  end
+end
+
+Generator.new(ARGV[0])
