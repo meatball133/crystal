b304e7ddb92e721bcaedada27aee99a982dbbd80
Carl <meatballsofmeatballs@gmail.com>
2023-01-04 16:56:22 +0100

    Rewrote the generator to work with liquid

--- bin/generator_help.cr
+++ bin/generator_help.cr
@@ -1,45 +1,21 @@
-require "ecr/macros"
+require "liquid"
 require "json"
 require "http/client"
-require "./generator"
 
 class GeneratorHelp
+  @tpl : String | Nil
+
   def initialize(exercise : String)
     response = HTTP::Client.get("https://raw.githubusercontent.com/exercism/problem-specifications/main/exercises/#{exercise}/canonical-data.json")
     @json = JSON.parse(response.body)
-    @first = true
+    file = File.read("./exercises/practice/#{exercise}/.meta/template.liquid")
+    ctx = Liquid::Context.new
+    ctx.set "json", @json
+    tpl = Liquid::Template.parse file
+    @tpl = tpl.render ctx
   end
 
-  def to_snake(input)
-    result = ""
-    input.each_char do |x|
-      result += x.upcase == x ? "_#{x.downcase}" : x
-    end
-    result
+  def to_s
+    @tpl
   end
-
-  def to_captlized(input)
-    result = ""
-    input = input.capitalize
-    capitalized = false
-    input.each_char do |x|
-      if x == '-'
-        capitalized = true
-      else
-        result += capitalized ? x.upcase : x
-        capitalized = false
-      end
-    end
-    result
-  end
-
-  def status
-    if @first
-      @first = false
-      return "it"
-    end
-    "pending"
-  end
-
-  ECR.def_to_s "./exercises/practice/resistor-color/.meta/template.ecr"
 end
