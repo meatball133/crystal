a09991146a685d36b31f58c299b5872a143a0137
Carl <meatballsofmeatballs@gmail.com>
2023-01-05 09:50:25 +0100

    it now checks so the exercise is in the config

--- bin/generator_help.cr
+++ bin/generator_help.cr
@@ -4,14 +4,15 @@ require "http/client"
 
 class GeneratorHelp
   @tpl : String | Nil
+  @json : JSON::Any
 
-  def initialize(exercise : String)
-    response = HTTP::Client.get("https://raw.githubusercontent.com/exercism/problem-specifications/main/exercises/#{exercise}/canonical-data.json")
-    @json = JSON.parse(response.body)
-    template_path = "./exercises/practice/#{exercise}/.meta/template.liquid"
+  def initialize(@exercise : String)
+    check_config()
+    @json = get_remote_files()
+    remove_tests(toml())
+    template_path = "./exercises/practice/#{@exercise}/.meta/template.liquid"
     raise "Template not found: #{template_path}" unless File.exists?(template_path)
-    template_file = File.read("./exercises/practice/#{exercise}/.meta/template.liquid")
-    remove_tests(toml(exercise))
+    template_file = File.read("./exercises/practice/#{@exercise}/.meta/template.liquid")
     ctx = Liquid::Context.new
     ctx.set "json", @json
     tpl = Liquid::Template.parse template_file
@@ -22,9 +23,9 @@ class GeneratorHelp
     @tpl
   end
 
-  def toml(exercise : String)
+  def toml()
     uuid = [] of String
-    path = "./exercises/practice/#{exercise}/.meta/tests.toml"
+    path = "./exercises/practice/#{@exercise}/.meta/tests.toml"
     raise "Toml not found: #{path}" unless File.exists?(path)
     File.read(path).split("\n").each do |line|
       line = line.strip
@@ -51,5 +52,19 @@ class GeneratorHelp
         end
       end
     end
+  end
+
+  def get_remote_files()
+
+    response = HTTP::Client.get("https://raw.githubusercontent.com/exercism/problem-specifications/main/exercises/#{@exercise}/canonical-data.json")
+    JSON.parse(response.body)
+  end
+
+  def check_config()
+    config_file = File.read("./config.json")
+    JSON.parse(config_file)["exercises"]["practice"].as_a.each do |x|
+      return true if @exercise == x["slug"]
+    end
+    raise "Couldn't find the exercise in the config.json.\nMake sure you use the same slug name as in the config file."
   end
 end
