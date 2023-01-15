68ecd8d94ba60ec5dfacfa7b6028b3ee33cdcecc
Carl <meatballsofmeatballs@gmail.com>
2023-01-15 22:41:14 +0100

    Made the ci work with ecr

--- bin/template.sh
+++ bin/template.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Test the spec files.
-# Regenerate the spec files from the liquid template and check that the newly generated spec is the same as the old spec.
+# Used to start the generator 
 
 # error prints an error to STDERR and exits.
 
 if [ -z "$1" ] ; then
-    echo "usage: ./bin/run-in-docker.sh exercise-slug "
+    echo "usage: ./bin/template.sh exercise-slug "
     exit 1
 fi
 
 slug="$1"
 
-cat "./exercises/practice/${slug}/.meta/template.ecr" > "./bin/templates/template.ecr"
+cat "./exercises/practice/${slug}/.meta/test_template.ecr" > "./bin/templates/template.ecr"
 
 if [ $# -eq 2 ] ; then
     crystal ./bin/generator.cr "${slug}" "${2%/}" || exit 1
