163cc7c3420d3a74518245c931a8feeb0bddfda8
Carl <meatballsofmeatballs@gmail.com>
2023-01-16 09:19:15 +0100

    updated exercises

--- exercises/practice/allergies/spec/allergies_spec.cr
+++ exercises/practice/allergies/spec/allergies_spec.cr
@@ -2,199 +2,203 @@ require "spec"
 require "../src/*"
 
 describe "Allergies" do
-  it "not allergic to anything" do
+  it "testing for eggs allergy" do
     Allergies.allergic_to("eggs", 0).should eq(false)
   end
 
-  pending "allergic only to eggs" do
+  pending "testing for eggs allergy" do
     Allergies.allergic_to("eggs", 1).should eq(true)
   end
 
-  pending "allergic to eggs and something else" do
+  pending "testing for eggs allergy" do
     Allergies.allergic_to("eggs", 3).should eq(true)
   end
 
-  pending "allergic to something, but not eggs" do
+  pending "testing for eggs allergy" do
     Allergies.allergic_to("eggs", 2).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for eggs allergy" do
     Allergies.allergic_to("eggs", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for peanuts allergy" do
     Allergies.allergic_to("peanuts", 0).should eq(false)
   end
 
-  pending "allergic only to peanuts" do
+  pending "testing for peanuts allergy" do
     Allergies.allergic_to("peanuts", 2).should eq(true)
   end
 
-  pending "allergic to peanuts and something else" do
+  pending "testing for peanuts allergy" do
     Allergies.allergic_to("peanuts", 7).should eq(true)
   end
 
-  pending "allergic to something, but not peanuts" do
+  pending "testing for peanuts allergy" do
     Allergies.allergic_to("peanuts", 5).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for peanuts allergy" do
     Allergies.allergic_to("peanuts", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for shellfish allergy" do
     Allergies.allergic_to("shellfish", 0).should eq(false)
   end
 
-  pending "allergic only to shellfish" do
+  pending "testing for shellfish allergy" do
     Allergies.allergic_to("shellfish", 4).should eq(true)
   end
 
-  pending "allergic to shellfish and something else" do
+  pending "testing for shellfish allergy" do
     Allergies.allergic_to("shellfish", 14).should eq(true)
   end
 
-  pending "allergic to something, but not shellfish" do
+  pending "testing for shellfish allergy" do
     Allergies.allergic_to("shellfish", 10).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for shellfish allergy" do
     Allergies.allergic_to("shellfish", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for strawberries allergy" do
     Allergies.allergic_to("strawberries", 0).should eq(false)
   end
 
-  pending "allergic only to strawberries" do
+  pending "testing for strawberries allergy" do
     Allergies.allergic_to("strawberries", 8).should eq(true)
   end
 
-  pending "allergic to strawberries and something else" do
+  pending "testing for strawberries allergy" do
     Allergies.allergic_to("strawberries", 28).should eq(true)
   end
 
-  pending "allergic to something, but not strawberries" do
+  pending "testing for strawberries allergy" do
     Allergies.allergic_to("strawberries", 20).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for strawberries allergy" do
     Allergies.allergic_to("strawberries", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for tomatoes allergy" do
     Allergies.allergic_to("tomatoes", 0).should eq(false)
   end
 
-  pending "allergic only to tomatoes" do
+  pending "testing for tomatoes allergy" do
     Allergies.allergic_to("tomatoes", 16).should eq(true)
   end
 
-  pending "allergic to tomatoes and something else" do
+  pending "testing for tomatoes allergy" do
     Allergies.allergic_to("tomatoes", 56).should eq(true)
   end
 
-  pending "allergic to something, but not tomatoes" do
+  pending "testing for tomatoes allergy" do
     Allergies.allergic_to("tomatoes", 40).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for tomatoes allergy" do
     Allergies.allergic_to("tomatoes", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for chocolate allergy" do
     Allergies.allergic_to("chocolate", 0).should eq(false)
   end
 
-  pending "allergic only to chocolate" do
+  pending "testing for chocolate allergy" do
     Allergies.allergic_to("chocolate", 32).should eq(true)
   end
 
-  pending "allergic to chocolate and something else" do
+  pending "testing for chocolate allergy" do
     Allergies.allergic_to("chocolate", 112).should eq(true)
   end
 
-  pending "allergic to something, but not chocolate" do
+  pending "testing for chocolate allergy" do
     Allergies.allergic_to("chocolate", 80).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for chocolate allergy" do
     Allergies.allergic_to("chocolate", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for pollen allergy" do
     Allergies.allergic_to("pollen", 0).should eq(false)
   end
 
-  pending "allergic only to pollen" do
+  pending "testing for pollen allergy" do
     Allergies.allergic_to("pollen", 64).should eq(true)
   end
 
-  pending "allergic to pollen and something else" do
+  pending "testing for pollen allergy" do
     Allergies.allergic_to("pollen", 224).should eq(true)
   end
 
-  pending "allergic to something, but not pollen" do
+  pending "testing for pollen allergy" do
     Allergies.allergic_to("pollen", 160).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for pollen allergy" do
     Allergies.allergic_to("pollen", 255).should eq(true)
   end
 
-  pending "not allergic to anything" do
+  pending "testing for cats allergy" do
     Allergies.allergic_to("cats", 0).should eq(false)
   end
 
-  pending "allergic only to cats" do
+  pending "testing for cats allergy" do
     Allergies.allergic_to("cats", 128).should eq(true)
   end
 
-  pending "allergic to cats and something else" do
+  pending "testing for cats allergy" do
     Allergies.allergic_to("cats", 192).should eq(true)
   end
 
-  pending "allergic to something, but not cats" do
+  pending "testing for cats allergy" do
     Allergies.allergic_to("cats", 64).should eq(false)
   end
 
-  pending "allergic to everything" do
+  pending "testing for cats allergy" do
     Allergies.allergic_to("cats", 255).should eq(true)
   end
 
-  pending "no allergies" do
+  pending "list when:" do
     Allergies.list(0).should eq([] of String)
   end
 
-  pending "just eggs" do
-    Allergies.list(1).should eq(["eggs"])
+  pending "list when:" do
+    Allergies.list(1).should eq(["eggs"] of String)
   end
 
-  pending "just peanuts" do
-    Allergies.list(2).should eq(["peanuts"])
+  pending "list when:" do
+    Allergies.list(2).should eq(["peanuts"] of String)
   end
 
-  pending "just strawberries" do
-    Allergies.list(8).should eq(["strawberries"])
+  pending "list when:" do
+    Allergies.list(8).should eq(["strawberries"] of String)
   end
 
-  pending "eggs and peanuts" do
-    Allergies.list(3).should eq(["eggs", "peanuts"])
+  pending "list when:" do
+    Allergies.list(3).should eq(["eggs", "peanuts"] of String)
   end
 
-  pending "more than eggs but not peanuts" do
-    Allergies.list(5).should eq(["eggs", "shellfish"])
+  pending "list when:" do
+    Allergies.list(5).should eq(["eggs", "shellfish"] of String)
   end
 
-  pending "lots of stuff" do
-    Allergies.list(248).should eq(["strawberries", "tomatoes", "chocolate", "pollen", "cats"])
+  pending "list when:" do
+    Allergies.list(248).should eq(["strawberries", "tomatoes", "chocolate", "pollen", "cats"] of String)
   end
 
-  pending "everything" do
-    Allergies.list(255).should eq(["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"])
+  pending "list when:" do
+    Allergies.list(255).should eq(["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"] of String)
   end
 
-  pending "no allergen score parts" do
-    Allergies.list(509).should eq(["eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"])
+  pending "list when:" do
+    Allergies.list(509).should eq(["eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"] of String)
+  end
+
+  pending "list when:" do
+    Allergies.list(257).should eq(["eggs"] of String)
   end
 end
