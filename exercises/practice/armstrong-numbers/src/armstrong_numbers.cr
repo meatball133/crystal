52bb2e57f85ac2e9c60b8feefe13f02fba0e632e
meatball <69751659+meatball133@users.noreply.github.com>
2023-01-19 16:04:54 +0000

    fix

--- exercises/practice/armstrong-numbers/src/armstrong_numbers.cr
+++ exercises/practice/armstrong-numbers/src/armstrong_numbers.cr
@@ -1,4 +1,3 @@
-# Please implement your solution to armstrong-numbers in this file
 module ArmstrongNumbers
   def self.armstrong_number?(input : Number) : Bool
     # Write your code for the 'Armstrong Numbers' exercise in this file.
