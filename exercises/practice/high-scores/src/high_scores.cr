4415942f325cdaad7d9d62943382d5c89ce60bad
Carl <meatballsofmeatballs@gmail.com>
2023-01-23 09:06:10 +0100

    Added stub file

--- exercises/practice/high-scores/src/high_scores.cr
+++ exercises/practice/high-scores/src/high_scores.cr
@@ -1,18 +1,3 @@
 class HighScores
-  property scores : Array(Int32)
-
-  def initialize(@scores : Array(Int32))
-  end
-
-  def latest : Int32
-    @scores.last
-  end
-
-  def personal_best : Int32
-    @scores.max
-  end
-
-  def personal_top_three : Array(Int32)
-    @scores.sort.reverse[0, 3]
-  end
+  # Write your code for the 'HighScores' exercise in this file.
 end
