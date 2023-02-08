71d48003240996f5b424663110e4c5bf076488ff
Carl <meatballsofmeatballs@gmail.com>
2023-02-08 09:50:32 +0100

    Fix tests

--- exercises/concept-wip/lasagna/spec/lasagna_test.cr
+++ exercises/concept-wip/lasagna/spec/lasagna_test.cr
@@ -21,25 +21,25 @@ describe Lasagna do
   end
 
   describe "preparation_time_in_minutes" do
-    it "1 layer should return 8" do
-      Lasagna.preparation_time_in_minutes(1).should eq(8)
+    it "1 layer should return 2" do
+      Lasagna.preparation_time_in_minutes(1).should eq(2)
     end
-    it "2 layers should return 16" do
-      Lasagna.preparation_time_in_minutes(2).should eq(16)
+    it "2 layers should return 4" do
+      Lasagna.preparation_time_in_minutes(2).should eq(4)
     end
-    it "4 layers should return 32" do
-      Lasagna.preparation_time_in_minutes(4).should eq(32)
+    it "4 layers should return 8" do
+      Lasagna.preparation_time_in_minutes(4).should eq(8)
     end
   end
 
   describe "total_time_in_minutes" do
     it "1 layer, 30 minutes in oven should return 32" do
-      Lasagna.total_time_in_minutes(1, 30).should eq(2)
+      Lasagna.total_time_in_minutes(1, 30).should eq(32)
     end
     it "2 layers, 25 minutes in oven should return 29" do
       Lasagna.total_time_in_minutes(2, 25).should eq(29)
     end
-    it "4 layers, 8 minutes in oven should return 57" do
+    it "4 layers, 8 minutes in oven should return 16" do
       Lasagna.total_time_in_minutes(4, 8).should eq(16)
     end
   end
