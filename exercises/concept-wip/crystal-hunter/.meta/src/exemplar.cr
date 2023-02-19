b43665df750414d881d7ed97e88c7065e8f6e212
Carl <meatballsofmeatballs@gmail.com>
2023-02-19 13:33:10 +0100

    Fixed exemplar and fix file name

---
+++ exercises/concept-wip/crystal-hunter/.meta/src/exemplar.cr
@@ -0,0 +1,17 @@
+class Rules
+  def eat_ghost?(power_pellet_active, touching_ghost)
+    power_pellet_active && touching_ghost
+  end
+
+  def score?(touching_power_pellet, touching_dot)
+    touching_power_pellet || touching_dot
+  end
+
+  def lose?(power_pellet_active, touching_ghost)
+    !power_pellet_active && touching_ghost
+  end
+
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost)
+    has_eaten_all_dots && !lose?(power_pellet_active, touching_ghost)
+  end
+end
