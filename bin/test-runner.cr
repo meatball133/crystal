cabc96b22659d3c90c19df5ab6ff52872edee930
Carl <meatballsofmeatballs@gmail.com>
2023-03-04 21:54:48 +0100

    attempt v2

--- bin/test-runner.cr
+++ bin/test-runner.cr
@@ -6,7 +6,7 @@ module Test
         Dir.mkdir("/tmp/spec/") unless Dir.exists?("/tmp/spec/")
         File.write("/tmp/src/#{dir.gsub("-", "_")}.cr", File.open("./exercises/practice/#{dir}/.meta/src/example.cr"))
         File.write("/tmp/spec/#{dir.gsub("-", "_")}_spec.cr", File.open("./exercises/practice/#{dir}/spec/#{dir.gsub("-", "_")}_spec.cr").gets_to_end.gsub("pending", "it"))
-        system("crystal /tmp/spec/#{dir.gsub("-", "_")}_spec.cr")
+        system("crystal spec /tmp/spec/#{dir.gsub("-", "_")}_spec.cr")
       end
     end
   end
