bba94692835972dcd3e5353b4b21b65103ee200c
Carl <meatballsofmeatballs@gmail.com>
2023-04-22 21:09:33 +0200

    Fixed path

--- bin/check-format.cr
+++ bin/check-format.cr
@@ -5,8 +5,8 @@ module Check_template
     Dir.open("./exercises/practice").each_child do |dir|
       json = JSON.parse(File.read("./exercises/practice/#{dir}/.meta/config.json"))
       File.write("/tmp/example.cr", File.read("./exercises/practice/#{dir}/.meta/src/example.cr"))
-      File.write("/tmp/src.cr", File.read("./exercises/practice/#{dir}/src/#{json["files"]["solution"][0]}.cr"))
-      File.write("/tmp/spec.cr", File.read("./exercises/practice/#{dir}/spec/#{json["files"]["test"][0]}_spec.cr"))
+      File.write("/tmp/src.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["solution"][0]}"))
+      File.write("/tmp/spec.cr", File.read("./exercises/practice/#{dir}/#{json["files"]["test"][0]}"))
 
       system("crystal tool format /tmp/example.cr")
       system("crystal tool format /tmp/src.cr")
@@ -14,10 +14,10 @@ module Check_template
       case
       when !File.same_content?("/tmp/example.cr", "./exercises/practice/#{dir}/.meta/src/example.cr")
         raise "The example file \"./exercises/practice/#{dir}/.meta/src/example.cr\" is not formatted"
-      when !File.same_content?("/tmp/src.cr", "./exercises/practice/#{dir}/src/#{json["files"]["solution"][0]}.cr")
-        raise "The source file \"./exercises/practice/#{dir}/src/#{json["files"]["solution"]}.cr\" is not formatted"
-      when !File.same_content?("/tmp/spec.cr", "./exercises/practice/#{dir}/spec/#{json["files"]["test"][0]}_spec.cr")
-        raise "The spec file \"./exercises/practice/#{dir}/spec/#{json["files"]["test"]}_spec.cr\" is not formatted"
+      when !File.same_content?("/tmp/src.cr", "./exercises/practice/#{dir}/#{json["files"]["solution"][0]}")
+        raise "The source file \"./exercises/practice/#{dir}/#{json["files"]["solution"]}\" is not formatted"
+      when !File.same_content?("/tmp/spec.cr", "./exercises/practice/#{dir}/#{json["files"]["test"][0]}")
+        raise "The spec file \"./exercises/practice/#{dir}/#{json["files"]["test"]}\" is not formatted"
       else
         puts "All files in \"./exercises/practice/#{dir}\" are formatted"
       end
@@ -29,8 +29,8 @@ module Check_template
       Dir.open("./exercises/concept").each_child do |dir|
         json = JSON.parse(File.read("./exercises/concept/#{dir}/.meta/config.json"))
         File.write("/tmp/exemplar.cr", File.read("./exercises/concept/#{dir}/.meta/src/exemplar.cr"))
-        File.write("/tmp/src.cr", File.read("./exercises/concept/#{dir}/src/#{json["files"]["solution"]}.cr"))
-        File.write("/tmp/spec.cr", File.read("./exercises/concept/#{dir}/spec/#{json["files"]["test"]}_spec.cr"))
+        File.write("/tmp/src.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["solution"][0]}"))
+        File.write("/tmp/spec.cr", File.read("./exercises/concept/#{dir}/#{json["files"]["test"][0]}"))
 
         system("crystal tool format /tmp/exemplar.cr")
         system("crystal tool format /tmp/src.cr")
@@ -38,10 +38,10 @@ module Check_template
         case
         when !File.same_content?("/tmp/exemplar.cr", "./exercises/concept/#{dir}/.meta/src/exemplar.cr")
           raise "The exemplar file \"./exercises/concept/#{dir}/.meta/src/exemplar.cr\" is not formatted"
-        when !File.same_content?("/tmp/src.cr", "./exercises/concept/#{dir}/src/#{json["files"]["solution"]}.cr")
-          raise "The source file \"./exercises/concept/#{dir}/src/#{json["files"]["solution"]}.cr\" is not formatted"
-        when !File.same_content?("/tmp/spec.cr", "./exercises/concept/#{dir}/spec/#{json["files"]["test"]}_spec.cr")
-          raise "The spec file \"./exercises/concept/#{dir}/spec/#{json["files"]["test"]}_spec.cr\" is not formatted"
+        when !File.same_content?("/tmp/src.cr", "./exercises/concept/#{dir}/#{json["files"]["solution"][0]}")
+          raise "The source file \"./exercises/concept/#{dir}/#{json["files"]["solution"]}\" is not formatted"
+        when !File.same_content?("/tmp/spec.cr", "./exercises/concept/#{dir}/#{json["files"]["test"][0]}")
+          raise "The spec file \"./exercises/concept/#{dir}/#{json["files"]["test"]}\" is not formatted"
         end
       end
     end
