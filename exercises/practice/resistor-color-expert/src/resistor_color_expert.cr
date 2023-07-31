210ec59b99002286ad04598a38a753106041a1a0
Carl <meatballsofmeatballs@gmail.com>
2023-07-31 21:11:37 +0200

    Fix formatting

--- exercises/practice/resistor-color-expert/src/resistor_color_expert.cr
+++ exercises/practice/resistor-color-expert/src/resistor_color_expert.cr
@@ -1,5 +1,5 @@
 module ResistorColorExpert
-    def self.resistor_label(bands : Array(String)) : String
-        # Write your code for the 'Resistor Color Expert' exercise in this file.
-    end
+  def self.resistor_label(bands : Array(String)) : String
+    # Write your code for the 'Resistor Color Expert' exercise in this file.
+  end
 end
