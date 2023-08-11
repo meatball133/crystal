c3ec3c449e7ffe02311c41241a2ec4f77631ed32
Carl <meatballsofmeatballs@gmail.com>
2023-08-11 15:37:40 +0200

    Add exercise and concept

---
+++ exercises/concept/chess-game/src/chess_game.cr
@@ -0,0 +1,16 @@
+module Chess
+  # TODO: define the 'RANKS' constant
+  # TODO: define the 'FILES' constant
+
+  def self.valid_square?(rank, file)
+    raise "Please implement the Lasagna.valid_square? method"
+  end
+
+  def self.nick_name(first_name, last_name)
+    raise "Please implement the Lasagna.nick_name method"
+  end
+
+  def self.move_message(first_name, last_name, square)
+    raise "Please implement the Lasagna.move_message method"
+  end
+end
