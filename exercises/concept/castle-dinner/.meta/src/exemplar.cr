6489653f0d39ad4aa1651db6bc28f724f9580bfb
Carl <meatballsofmeatballs@gmail.com>
2023-08-13 23:13:54 +0200

    Started

---
+++ exercises/concept/castle-dinner/.meta/src/exemplar.cr
@@ -0,0 +1,17 @@
+class CastleDinner
+  def self.check_food?(food)
+    food == "Mushroom pasties" ? food : nil
+  end
+
+  def self.check_drink?(drink)
+    has_i = drink.downcase.include?("i")
+    unless has_i.nil?
+      return drink
+    end
+    nil
+  end
+
+  def self.replace_drink(drink)
+    check_drink?(drink) || "Apple juice"
+  end
+end
