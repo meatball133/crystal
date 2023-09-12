83c59bb8f11ae4294eb5415b55e36111e853a363
Carl <meatballsofmeatballs@gmail.com>
2023-09-12 21:18:24 +0200

    Fixes

--- exercises/concept/chess-game/src/chess_game.cr
+++ exercises/concept/chess-game/src/chess_game.cr
@@ -3,14 +3,14 @@ module Chess
   # TODO: define the 'FILES' constant
 
   def self.valid_square?(rank, file)
-    raise "Please implement the Lasagna.valid_square? method"
+    raise "Please implement the Chess.valid_square? method"
   end
 
   def self.nick_name(first_name, last_name)
-    raise "Please implement the Lasagna.nick_name method"
+    raise "Please implement the Chess.nick_name method"
   end
 
   def self.move_message(first_name, last_name, square)
-    raise "Please implement the Lasagna.move_message method"
+    raise "Please implement the Chess.move_message method"
   end
 end
