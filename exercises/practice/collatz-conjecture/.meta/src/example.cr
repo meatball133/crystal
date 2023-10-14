9d042a00b639df6ac00606903611b3cd6c30efd0
Carl <meatballsofmeatballs@gmail.com>
2023-10-14 22:52:13 +0200

    Simplify example

--- exercises/practice/collatz-conjecture/.meta/src/example.cr
+++ exercises/practice/collatz-conjecture/.meta/src/example.cr
@@ -7,13 +7,13 @@ module CollatzConjecture
     n = input
     steps = 0
     until n == 1
-      if n % 2 == 0
-        n = n / 2
+      if n.divisible_by?(2)
+        n /= 2
       else
         n = 3 * n + 1
       end
 
-      steps = steps + 1
+      steps += steps
     end
 
     steps
