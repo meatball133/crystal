c4ce0ca9be6f33490f35ceeed67c2cf925abb89a
Carl <meatballsofmeatballs@gmail.com>
2024-01-15 22:51:07 +0100

    Add ci

--- test-generator/spec/formatter_tests.cr
+++ test-generator/spec/formatter_tests.cr
@@ -4,18 +4,16 @@ require "compiler/crystal/formatter"
 
 describe "Format" do
   it "format files correctley" do
-    content = File.read("./test-generator/spec/assets/format-files/format_file.cr")
-    content = Crystal.format(content)
-    content = Formatter.post_proccesing(content)
+    system("crystal tool format ./test-generator/spec/assets/format-files/format_file.cr")
+    content = Formatter.post_proccesing("./test-generator/spec/assets/format-files/format_file.cr")
     content = Crystal.format(content)
     
     content.should eq(File.read("./test-generator/spec/assets/format-files/expected_format_file.cr"))
   end
 
   it "format etl correctley" do
-    content = File.read("./test-generator/spec/assets/format-files/etl.cr")
-    content = Crystal.format(content)
-    content = Formatter.post_proccesing(content)
+    system("crystal tool format ./test-generator/spec/assets/format-files/format_file.cr")
+    content = Formatter.post_proccesing("./test-generator/spec/assets/format-files/etl.cr")
     content = Crystal.format(content)
     
     content.should eq(File.read("./test-generator/spec/assets/format-files/expected_etl.cr"))
