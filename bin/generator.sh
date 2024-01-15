265ea4ce6ddfa67822ac6fc4d147a6f47c3b4fe7
Carl <meatballsofmeatballs@gmail.com>
2024-01-15 22:40:02 +0100

    Write format spec and update the formatter

--- bin/generator.sh
+++ bin/generator.sh
@@ -18,7 +18,7 @@ fi
 cat "./exercises/practice/${slug}/.meta/test_template.ecr" > "./bin/templates/template.ecr"
 
 if [ $# -eq 2 ] ; then
-    crystal ../test-generator/generator.cr "${slug}" "${2%/}" || exit 1
+    crystal ./test-generator/src/generator.cr "${slug}" "${2%/}" || exit 1
 else
-    crystal ../test-generator/generator.cr "${slug}" || exit 1
+    crystal ./test-generator/src/generator.cr "${slug}" || exit 1
 fi
--- test-generator/spec/formatter_tests.cr
+++ test-generator/spec/formatter_tests.cr
@@ -1,14 +1,23 @@
 require "spec"
-require "../formatter"
+require "../src/formatter"
+require "compiler/crystal/formatter"
 
-describe "Generator" do
-  describe "format" do
-    it "format files correctley" do
-      temp_file = File.tempfile("temp")
-      File.write(temp_file.path, Formatter.post_proccesing("./bin/test-templates/files-to-format/example.cr"))
-      system("crystal tool format #{temp_file.path}")
-      File.read(temp_file.path).should eq(File.read("./bin/test-templates/formatted-files/example.cr"))
-      temp_file.delete
-    end
+describe "Format" do
+  it "format files correctley" do
+    content = File.read("./test-generator/spec/assets/format-files/format_file.cr")
+    content = Crystal.format(content)
+    content = Formatter.post_proccesing(content)
+    content = Crystal.format(content)
+    
+    content.should eq(File.read("./test-generator/spec/assets/format-files/expected_format_file.cr"))
+  end
+
+  it "format etl correctley" do
+    content = File.read("./test-generator/spec/assets/format-files/etl.cr")
+    content = Crystal.format(content)
+    content = Formatter.post_proccesing(content)
+    content = Crystal.format(content)
+    
+    content.should eq(File.read("./test-generator/spec/assets/format-files/expected_etl.cr"))
   end
 end
