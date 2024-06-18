ab833108115e2cec143a4e2d2bcab099cb93ed65
Ryan Hartlage <ryanplusplus@gmail.com>
2024-06-18 18:48:41 -0400

    Apply suggestions from code review

--- exercises/practice/game-of-life/.meta/src/example.cr
+++ exercises/practice/game-of-life/.meta/src/example.cr
@@ -7,7 +7,7 @@ module GameOfLife
         sub_array = board[({index_row - 1, 0}.max)..(index_row + 1)].map do |row|
           row[({index_cell - 1, 0}.max)..(index_cell + 1)]
         end
-        cells_around = check_sourding_squares(sub_array, {index_row.zero? ? 0 : 1, index_cell.zero? ? 0 : 1})
+        cells_around = check_surrounding_squares(sub_array, {index_row.zero? ? 0 : 1, index_cell.zero? ? 0 : 1})
         if cell == 1
           if 2 == cells_around.size || cells_around.size == 3
             result[index_row] << 1
@@ -26,7 +26,7 @@ module GameOfLife
     result
   end
 
-  private def self.check_sourding_squares(board, center)
+  private def self.check_surrounding_squares(board, center)
     result = [] of Int32
     board.each_with_index do |row, index_row|
       row.each_with_index do |cell, index_cell|
