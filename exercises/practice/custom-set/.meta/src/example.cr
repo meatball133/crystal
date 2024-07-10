9d6266b5e223c1d5471f04cb1f47983328a70c24
Meatball <meatballsofmeatballs@gmail.com>
2024-07-10 11:37:35 +0200

    More work

--- exercises/practice/custom-set/.meta/src/example.cr
+++ exercises/practice/custom-set/.meta/src/example.cr
@@ -1,8 +1,53 @@
-class CustomSet
+struct CustomSet
+    @elements : Hash(Int32, Nil)
+
+    getter elements
+
     def initialize(elements : Array(Int32))
-        @elements = Hash(Int32, Nil).new(elements.size) { |h, k| nil }
-        p @elements
+        @elements = elements.to_h { |element| {element, nil} }
+    end
+
+    def empty?
+        @elements.empty?
+    end
+
+    def contains?(element)
+        @elements.has_key?(element)
+    end
+
+    def subset?(other)
+        @elements.all? { |element, _| other.contains?(element) }
+    end
+
+    def disjoint?(other)
+        @elements.none? { |element, _| other.contains?(element) }
+    end
+
+    def equal?(other)
+        @elements.size == other.elements.size && subset?(other)
+    end
+
+    def add(element)
+        @elements[element] = nil
+        self
+    end
+
+    def ==(other)
+        equal?(other)
+    end
+
+    def intersection(other)
+        @elements.select! { |element, _| other.contains?(element) }
+        self
+    end
+
+    def difference(other)
+        @elements.reject! { |element, _| other.contains?(element) }
+        self
+    end
+
+    def union(other)
+        other.elements.each { |element, _| add(element) }
+        self
     end
 end
-
-p CustomSet.new([1, 2, 3, 4, 5])
