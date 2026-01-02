89ffdb7d1e9a36f21df3baccc6d5e19a31e3b185
meatball <meatballsofmeatballs@gmail.com>
2026-01-02 19:19:47 +0100

    Add Baffling Birthdays exercise with instructions, example, and tests

---
+++ exercises/practice/baffling-birthdays/.meta/src/example.cr
@@ -0,0 +1,29 @@
+class BafflingBirthdays
+  def self.shared_birthday(birthdates : Array(Time)) : Bool
+    birthdates.each_with_index do |date, idx|
+      birthdates.each_with_index do |date_2, idx_2|
+        if idx == idx_2
+          next
+        end
+        if date.month == date_2.month && date.day == date_2.day
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def self.random_birthdates(size : Int) : Array(Time)
+    result = [] of Time
+    size.times do |_|
+      time = Time.utc(2025, 1, 1).shift days: Random.rand(356)
+      result << time
+    end
+
+    return result
+  end
+
+  def self.estimated_probability_of_shared_birthday(size : Int) : Float
+    return (1..10000).sum {|_| shared_birthday(random_birthdates(size)) ? 1 : 0} / 100.0
+  end
+end
