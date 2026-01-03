b01520cdd633affd18eeac9a24bdf72574c110e7
meatball <meatballsofmeatballs@gmail.com>
2026-01-03 13:42:39 +0100

    Format files

--- exercises/practice/parallel-letter-frequency/.meta/src/example.cr
+++ exercises/practice/parallel-letter-frequency/.meta/src/example.cr
@@ -1,29 +1,29 @@
 class ParallelLetterFrequency
-    def self.calculate_frequencies(input : Array(String)) : Hash(String, Int32)
-        frequencies = Hash(String, Int32).new(0)
-        channel = Channel(Hash(String, Int32)).new
+  def self.calculate_frequencies(input : Array(String)) : Hash(String, Int32)
+    frequencies = Hash(String, Int32).new(0)
+    channel = Channel(Hash(String, Int32)).new
 
-        input.each do |text|
-            spawn do
-                local = Hash(String, Int32).new(0)
-                processed = 0
+    input.each do |text|
+      spawn do
+        local = Hash(String, Int32).new(0)
+        processed = 0
 
-                text.each_char do |char|
-                    next unless char.letter?
-                    local[char.downcase.to_s] += 1
+        text.each_char do |char|
+          next unless char.letter?
+          local[char.downcase.to_s] += 1
 
-                    # Crystal fibers are cooperatively scheduled; yielding occasionally
-                    # prevents one long-running fiber from starving the others.
-                end
-                channel.send(local)
-            end
+          # Crystal fibers are cooperatively scheduled; yielding occasionally
+          # prevents one long-running fiber from starving the others.
         end
-        input.size.times do |x|
-            hash = channel.receive
-            frequencies.merge!(hash) do |key, oldval, newval|
-                oldval + newval
-            end
-        end
-        frequencies
+        channel.send(local)
+      end
     end
+    input.size.times do |x|
+      hash = channel.receive
+      frequencies.merge!(hash) do |key, oldval, newval|
+        oldval + newval
+      end
+    end
+    frequencies
+  end
 end
--- exercises/practice/parallel-letter-frequency/src/parallel_letter_frequency.cr
+++ exercises/practice/parallel-letter-frequency/src/parallel_letter_frequency.cr
@@ -1,5 +1,5 @@
 class ParallelLetterFrequency
-    def self.calculate_frequencies(input : Array(String)) : Hash(String, Int32)
-        # Write your code for the 'ParallelLetterFrequency' exercise in this file.
-    end
+  def self.calculate_frequencies(input : Array(String)) : Hash(String, Int32)
+    # Write your code for the 'ParallelLetterFrequency' exercise in this file.
+  end
 end
